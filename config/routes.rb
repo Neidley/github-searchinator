0c054070fc07cf7abb119596b599564f16c4e7cd
Andrew Neidley <neidz44@gmail.com>
2018-09-23 21:56:19 -0500

    omniauth github working, add bootstrap and jquery to gemfile, application.scss and application.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'session/create'
-
-  get 'session/destroy'
+  root 'welcome#about'
 
   get 'welcome/index'
 
   get 'welcome/about'
+
+  get 'auth/developer', :as => 'developer_auth'
+  get 'auth/github', :as => 'github_auth'
+
+  match 'auth/:provider/callback' => 'session#create', :via => [:get, :post]
+
+  get 'session/destroy', :as => 'logout'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
